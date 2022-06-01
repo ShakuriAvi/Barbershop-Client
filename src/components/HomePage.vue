66ef749627dbd54f68c6aafa392ba659c2749edb
ShakuriAvi <shakuri.avi96@gmail.com>
2022-06-01 22:15:52 +0300

    Add bootstrap and Datepicker

---
+++ src/components/HomePage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+
+    <Datepicker v-model="date" :format="format" />
+  </div>
+</template>
+
+<script>
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+// import { ref } from 'vue';
+
+export default {
+  name: 'HomePage',
+  components: { Datepicker },
+  data() {
+    return {
+      date: null
+    }
+  },
+  setup() {
+    // In case of a range picker, you'll receive [Date, Date]
+    const format = (date) => {
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+
+      return `Selected date is ${day}/${month}/${year}`;
+    }
+
+    return {
+      format,
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
